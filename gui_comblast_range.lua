baf27d9a4166bd29d37217b30ce6d145f0abc07c
decay <alexlpt@yandex.ru>
2013-10-08 02:13:57 +0400

    True dgun radius added

--- gui_comblast_range.lua
+++ gui_comblast_range.lua
@@ -1,14 +1,14 @@
 function widget:GetInfo()
-  return {
-    name      = "Comblast Range v5",
-    desc      = "Draws a circle that displays commander blast range",
-    author    = "TheFatController",
-    date      = "January 24, 2009",
-    license   = "MIT/X11",
-    layer     = 0,
-    version   = 5,
-    enabled   = true  -- loaded by default
-  }
+    return {
+        name      = "Comblast Range v6",
+        desc      = "Draws a circle that displays commander blast range",
+        author    = "TheFatController",
+        date      = "January 24, 2009",
+        license   = "MIT/X11",
+        layer     = 0,
+        version   = 6,
+        enabled   = true  -- loaded by default
+    }
 end
 
 -- project page on github: https://github.com/jamerlan/gui_comblast_range
@@ -18,6 +18,7 @@ end
 -- v3 [teh]decay Draw circles for visible enemy commanders too! +Fix spectator mode when /fullspecview is used + code speedup
 -- v4 [teh]decay Some minor improvements and fixes
 -- v5 [teh]decay fix spectator mode again (thx to jK)
+-- v6 [teh]decay True dgun radius added
 
 local GetUnitPosition     = Spring.GetUnitPosition
 local glDrawGroundCircle  = gl.DrawGroundCircle
@@ -37,83 +38,82 @@ local explodeTag = "deathExplosion"
 local selfdTag = "selfDExplosion"
 local aoeTag = "damageAreaOfEffect"
 
+local dgunRange = WeaponDefNames["armcom_arm_disintegrator"].range + WeaponDefNames["armcom_arm_disintegrator"].damageAreaOfEffect
+
 local commanders = {}
 
 local spectatorMode = false
 local notInSpecfullmode = false
 
 function widget:UnitFinished(unitID, unitDefID, unitTeam)
-  if UnitDefs[unitDefID].canManualFire then
-    commanders[unitID] = true
-  end
+    if UnitDefs[unitDefID].canManualFire then
+        commanders[unitID] = true
+    end
 end
 
 function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
-  if commanders[unitID] then
-    commanders[unitID] = nil
-  end
+    if commanders[unitID] then
+        commanders[unitID] = nil
+    end
 end
 
 function widget:UnitEnteredLos(unitID, unitTeam)
     if not spectatorMode then
         local unitDefID = GetUnitDefID(unitID)
         if UnitDefs[unitDefID].canManualFire then
-          commanders[unitID] = true
+            commanders[unitID] = true
         end
     end
 end
 
 function widget:UnitLeftLos(unitID, unitDefID, unitTeam)
     if not spectatorMode then
-      if commanders[unitID] then
-        commanders[unitID] = nil
-      end
+        if commanders[unitID] then
+            commanders[unitID] = nil
+        end
     end
 end
 
 function widget:DrawWorldPreUnit()
-  local _, specFullView, _ = spGetSpectatingState()
+    local _, specFullView, _ = spGetSpectatingState()
 
-  if not
-
-
-
-
-
-  specFullView then
-      notInSpecfullmode = true
-  else
-      if notInSpecfullmode then
-          detectSpectatorView()
-      end
-      notInSpecfullmode = false
-  end
-
-  gl.DepthTest(true)
-
-  for unitID in pairs(commanders) do
-    local x,y,z = GetUnitPosition(unitID)
-    local udefId = GetUnitDefID(unitID);
-    if udefId ~= nil then
-        local udef = udefTab[udefId]
-        local blastRadius = 380;
-
-        local deathBlastId = weapNamTab[lower(udef[explodeTag])].id
-        local selfdBlastId = weapNamTab[lower(udef[selfdTag])].id
-
-        local deathBlastRadius = weapTab[deathBlastId][aoeTag]
-        local selfdBlastRadius = weapTab[selfdBlastId][aoeTag]
-
-        if deathBlastRadius ~= selfdBlastRadius then
-            gl.Color(1, 0, 0, .5)
-            glDrawGroundCircle(x, y, z, deathBlastRadius, blastCircleDivs)
+    if not specFullView then
+        notInSpecfullmode = true
+    else
+        if notInSpecfullmode then
+            detectSpectatorView()
         end
-
-        gl.Color(1, 1, 0, .5)
-        glDrawGroundCircle(x, y, z, selfdBlastRadius, blastCircleDivs)
+        notInSpecfullmode = false
     end
-  end
-  gl.DepthTest(false)
+
+    gl.DepthTest(true)
+
+    for unitID in pairs(commanders) do
+        local x,y,z = GetUnitPosition(unitID)
+        local udefId = GetUnitDefID(unitID);
+        if udefId ~= nil then
+            local udef = udefTab[udefId]
+            local blastRadius = 380;
+
+            local deathBlastId = weapNamTab[lower(udef[explodeTag])].id
+            local selfdBlastId = weapNamTab[lower(udef[selfdTag])].id
+
+            local deathBlastRadius = weapTab[deathBlastId][aoeTag]
+            local selfdBlastRadius = weapTab[selfdBlastId][aoeTag]
+
+            if deathBlastRadius ~= selfdBlastRadius then
+                gl.Color(1, 0, 0, .5)
+                glDrawGroundCircle(x, y, z, deathBlastRadius, blastCircleDivs)
+            end
+
+            gl.Color(1, 1, 0, .5)
+            glDrawGroundCircle(x, y, z, selfdBlastRadius, blastCircleDivs)
+
+            gl.Color(0, 0, 1, .5)
+            glDrawGroundCircle(x, y, z, dgunRange, blastCircleDivs)
+        end
+    end
+    gl.DepthTest(false)
 end
 
 function widget:PlayerChanged(playerID)
